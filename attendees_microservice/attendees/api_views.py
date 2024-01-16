018d3adcc9097fff9100dd4c49e4d0cb29b4e4a9
noahD <noahharshb@gmail.com>
2024-01-16 16:58:49 -0700

    Microservices

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -1,12 +1,13 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 import json
-
-from events.api_views import ConferenceListEncoder
+from .models import ConferenceVO
 from common.json import ModelEncoder
 from .models import Attendee
-from events.models import Conference
 
+class ConferenceVODetailEncoder(ModelEncoder):
+    model = ConferenceVO
+    properties = ["name", "import_href"]
 
 class AttendeeListEncoder(ModelEncoder):
     model = Attendee
@@ -23,12 +24,12 @@ class AttendeeDetailEncoder(ModelEncoder):
         "conference",
     ]
     encoders = {
-        "conference": ConferenceListEncoder(),
+        "conference": ConferenceVODetailEncoder(),
     }
 
 
 @require_http_methods(["GET", "POST"])
-def api_list_attendees(request, conference_id):
+def api_list_attendees(request, conference_vo_id=None):
     """
     Lists the attendees names and the link to the attendee
     for the specified conference id.
@@ -49,7 +50,7 @@ def api_list_attendees(request, conference_id):
     }
     """
     if request.method == "GET":
-        attendees = Attendee.objects.filter(conference=conference_id)
+        attendees = Attendee.objects.filter(conference=conference_vo_id)
         return JsonResponse(
             {"attendees": attendees},
             encoder=AttendeeListEncoder,
@@ -59,9 +60,10 @@ def api_list_attendees(request, conference_id):
 
         # Get the Conference object and put it in the content dict
         try:
-            conference = Conference.objects.get(id=conference_id)
+            conference_href = f'/api/conferences/{conference_vo_id}'
+            conference = ConferenceVO.objects.get(import_href=conference_vo_id)
             content["conference"] = conference
-        except Conference.DoesNotExist:
+        except ConferenceVO.DoesNotExist:
             return JsonResponse(
                 {"message": "Invalid conference id"},
                 status=400,
